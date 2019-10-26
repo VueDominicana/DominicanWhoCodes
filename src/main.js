3b551b7522b80698007f47ccb97e7eb0ce375e42
jews1 <jesws1@gmail.com>
2019-10-26 00:27:09 -0400

    Add dependency to run the proyect outsite det

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/style/custom.css';
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
