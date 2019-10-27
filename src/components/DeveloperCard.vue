5a2e0ef4d61d917d48f3ba1409d5b5b1498f0bbc
freesgen <jesusant.guerrero@gmail.com>
2019-10-27 13:08:45 -0400

    improve card styles

--- src/components/DeveloperCard.vue
+++ src/components/DeveloperCard.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="card developer-card mb-3">
-    <div class="h-100"><img class="card-img-top" :src="developer.image" alt="Card image cap"></div>
-    <div class="card-body p-3">
-        <div class="d-flex align-items-center info-space">
-            <div>
-                <span class="avatar avatar-sm rounded-circle bg-dark text-white">{{ developer.initials }}</span>
-            </div>
-            <div class="pl-3"><span class="h5 text-sm mb-0">{{ developer.name }}</span></div>
+<div class="developer-card card mb-3">
+    <div class="developer-card__image-container">
+        <img class="developer-card__image-cover" :src="image" alt="developer image">
+    </div>
 
+    <div class="developer-card__body card-body p-3">
+        <div class="developer-card__header d-flex align-items-center">
+            <div>
+                <span class="developer-card__avatar avatar-sm rounded-circle bg-dark text-white">{{ developer.initials }}</span>
+            </div>
+            <div class="pl-3"><span class="developer-card__name h6 text-sm mb-0">{{ developer.name }}</span></div>
         </div>
-        <section class="info-skills" ref="skills">
+
+        <section class="developer-card__skills" ref="skills">
             <div class="info-space">
                 <span v-for="skill in skillList" :key="skill" class="badge badge-dark badge-pill">{{ skill }}</span>
                 <span v-if="haveMoreSkills" class="badge badge-dark badge-pill"
@@ -19,12 +22,14 @@
                 </span>
             </div>
         </section>
-        <div class="description-space">
-            <p class="text-muted text-monospace ">
+
+        <div class="developer-card__description">
+            <p class="text-muted text-monospace">
                 <small>{{ developer.summary }}</small>
             </p>
         </div>
-        <div class="social-networks">
+
+        <div class="developer-card__social-networks">
             <a v-if="developer.webpage" :href="developer.webpage" target="_blank" class="text-dark"><font-awesome-icon icon="globe-americas" /></a>
             <a v-if="developer.linkedIn" :href="developer.linkedIn" target="_blank" class="text-dark"><font-awesome-icon :icon="['fab', 'linkedin']" /></a>
             <a v-if="developer.twitter" :href="developer.twitter" target="_blank" class="text-dark"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
@@ -62,12 +67,10 @@ export default {
             showAll: false
         }
     },
-
     mounted() {
         // we need know if the developer have more than a row of skills to show the toggle
         this.skillsHeight = this.$refs.skills.clientHeight;
     },
-    
     computed: {
         skills() {
             return this.developer.skills.split(',').map(skill => skill.trim());
@@ -90,10 +93,14 @@ export default {
             }
             return 0
         },
+
+        image() {
+            const localImage = "data/devs-pics/";
+            return this.developer.image.replace(localImage,`http://www.dominicanwho.codes/${localImage}`)
+        }
     }
 }
 </script>
-
 
 <style lang="scss" scoped>
 
@@ -102,70 +109,86 @@ export default {
     border-radius: 0.5rem;
     box-shadow: 0 0.75rem 1.5rem rgba(0, 0, 0, 0.03);
     transition: all ease .3s;
+    width: 255px;
     cursor: pointer;
+    max-height: 530px;
+    height: 100%;
+    overflow: hidden;
 
     &:hover {
-        transform: scale(1.05);
+        transform: scale(1.02);
         z-index: 1000;
         box-shadow: 3px 3px 10px -2px rgba(0, 0, 0, 0.3);
 
-
-        .card-img-top{
+        .developer-card__image-cover{
             filter: none;
         }
     }
-}
 
-.card-img-top {
-    border-top-left-radius: calc(0.5rem - 1px);
-    border-top-right-radius: calc(0.5rem - 1px);
-    filter: grayscale(100%);
-    transition: all 375ms ease-in-out;
-    width: 100%;
-    height: 15vw;
-    object-fit: cover;
-    object-position: 0 -10px;
-}
+    &__image-cover {
+        border-top-left-radius: calc(0.5rem - 1px);
+        border-top-right-radius: calc(0.5rem - 1px);
+        filter: grayscale(100%);
+        transition: all 375ms ease-in-out;
+        width: 100%;
+        height: 15vw;
+        object-fit: cover;
+        object-position: 0 -10px;
+    }
 
+    &__body {
+        text-align: left;
+    }
 
-.avatar {
-    position: relative;
-    color: #fff;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    vertical-align: middle;
-    font-size: 1rem;
-    font-weight: 600;
-    height: 3.125rem;
-    width: 3.125rem;
-    border-radius: 0.375rem;
-}
+    &__header {
+        margin-bottom: 2px;
+    }
 
-.info-skills {
-    margin: 0.3rem 0;
-    min-height: 35px;
-    overflow: hidden;
-    transition: all ease .3s;
+    &__avatar {
+        position: relative;
+        color: #fff;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: middle;
+        font-size: 1rem;
+        font-weight: 600;
+        height: 3.125rem;
+        width: 3.125rem;
+        border-radius: 0.375rem;
+    }
 
-    .info-space .badge {
-        margin: 2px 2px;
-        padding: 10px;
+    &__name {
+        display: flex;
+        text-align: left;
+    }
+
+    &__skills {
+        margin: 0.3rem 0;
+        min-height: 35px;
+        overflow: hidden;
+        transition: all ease .3s;
+
+        .info-space .badge {
+            margin: 2px 2px;
+            padding: 5px;
+        }
+    }
+
+    &__description {
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        overflow: hidden;
+        scrollbar-width: thin;
+    }
+
+    &__social-networks {
+        font-size: x-large;
+        display: flex;
+        justify-content: space-around;
     }
 }
 
-.description-space {
-    height: 100px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    overflow: hidden;
-    scrollbar-width: thin;
-}
-
-.social-networks {
-    font-size: x-large;
-    display: flex;
-    justify-content: space-around;
-}
 </style>
