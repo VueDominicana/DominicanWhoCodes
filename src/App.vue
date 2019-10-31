21fb972d6ecc09801578289d1ded6c1e730a294b
Danny Feliz <dannyfeliz08@gmail.com>
2019-10-31 19:13:47 -0400

    Remove nativation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <Header/>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/card-example">Card Example</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
