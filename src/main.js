d6cb006687c3752da7b8352f9c6b912579e7b2d3
Danny Feliz <dannyfeliz08@gmail.com>
2019-10-31 19:12:09 -0400

    Add config file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./config"
 import "./config/libs/bootstrap";
 import "./config/libs/fa-icons";
 import BootstrapVue from 'bootstrap-vue'
