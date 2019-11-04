dd089b9ca75453408e941a35baf97afa1cb57a15
Benjamin Vargas <bvj2206@gmail.com>
2019-11-04 04:12:52 -0400

    Change header buttons structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,8 +6,8 @@
     <b-collapse id="nav-collapse" is-nav>
     <b-navbar-nav class="ml-auto">
      <b-navbar-nav right>
-        <b-nav-item class="text-dark font-weight-bold" href="https://github.com/VueDominicana/DominicanWhoCodes" target="_blank"><font-awesome-icon :icon="['fab', 'github']"/> About</b-nav-item>
-        <b-nav-item href="#">Nominate</b-nav-item>
+        <b-nav-item class="font-weight-bold active" href="https://github.com/VueDominicana/DominicanWhoCodes" target="_blank"><font-awesome-icon :icon="['fab', 'github']"/> About</b-nav-item>
+        <li class="nav-item"><b-button href="#" variant="dark" class="font-weight-bold">Nominate</b-button></li>
       </b-navbar-nav>
     </b-navbar-nav>
     </b-collapse>
