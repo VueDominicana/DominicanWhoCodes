c0b11972cd9465b8abcb7c562c946f15def9540f
Lewandy Dilone <lewandydilone1@live.com>
2019-11-10 21:20:12 -0400

    convert all indents to spaces

--- src/views/DevelopersList.vue
+++ src/views/DevelopersList.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="home container">
-		<div v-if="isLoading">
-			<loading-screen></loading-screen>
-		</div>
-		<div v-else class="developers-container">
-			<developer-card v-for="(developer, index) in developers" :developer="developer" :key="index" />
-		</div>
-	</div>
+    <div class="home container">
+        <div v-if="isLoading">
+            <loading-screen></loading-screen>
+        </div>
+        <div v-else class="developers-container">
+            <developer-card v-for="(developer, index) in developers" :developer="developer" :key="index" />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -14,72 +14,72 @@ import DeveloperCard from "@/components/DeveloperCard.vue";
 import LoadingScreen from "@/components/LoadingScreen.vue";
 
 export default {
-	name: "DevelopersList",
-	components: {
-		DeveloperCard,
-		LoadingScreen
-	},
-	data() {
-		return {
-			developers: [],
-			isLoading: false
-		};
-	},
-	created() {
-		this.fetchDevelopers();
-	},
-	methods: {
-		fetchDevelopers() {
-			this.isLoading = true;
-			axios
-				.get("/developers")
-				.then(({ data: developers }) => {
-					this.sortDevelopers(developers);
-				})
-				.catch(error => {
-					console.error(error);
-				})
-				.finally(() => {
-					this.isLoading = false;
-				});
-		},
-		sortDevelopers(developers) {
-			const sortedDevelopers = developers.sort((a, b) =>
-				a.name.localeCompare(b.name, "es", { ignorePunctuation: true })
-			);
+    name: "DevelopersList",
+    components: {
+        DeveloperCard,
+        LoadingScreen
+    },
+    data() {
+        return {
+            developers: [],
+            isLoading: false
+        };
+    },
+    created() {
+        this.fetchDevelopers();
+    },
+    methods: {
+        fetchDevelopers() {
+            this.isLoading = true;
+            axios
+                .get("/developers")
+                .then(({ data: developers }) => {
+                    this.sortDevelopers(developers);
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        },
+        sortDevelopers(developers) {
+            const sortedDevelopers = developers.sort((a, b) =>
+                a.name.localeCompare(b.name, "es", { ignorePunctuation: true })
+            );
 
-			this.developers = sortedDevelopers;
-		}
-	}
+            this.developers = sortedDevelopers;
+        }
+    }
 };
 </script>
 
 <style lang="scss" scope>
 .developers-container {
-	display: grid;
-	flex-wrap: wrap;
-	grid-template-columns: repeat(4, 1fr);
-	grid-column-gap: 15px;
-	grid-row-gap: 15px;
-	justify-content: center;
-	transition: all ease 0.3s;
+    display: grid;
+    flex-wrap: wrap;
+    grid-template-columns: repeat(4, 1fr);
+    grid-column-gap: 15px;
+    grid-row-gap: 15px;
+    justify-content: center;
+    transition: all ease 0.3s;
 }
 
 @media (min-width: 768px) and (max-width: 991.98px) {
-	.developers-container {
-		grid-template-columns: repeat(auto-fill, minmax(220px, 220px));
-	}
+    .developers-container {
+        grid-template-columns: repeat(auto-fill, minmax(220px, 220px));
+    }
 }
 
 @media (min-width: 576px) and (max-width: 767px) {
-	.developers-container {
-		grid-template-columns: repeat(auto-fill, minmax(210px, 210px));
-	}
+    .developers-container {
+        grid-template-columns: repeat(auto-fill, minmax(210px, 210px));
+    }
 }
 
 @media screen and (max-width: 575px) {
-	.developers-container {
-		grid-template-columns: 0.85fr;
-	}
+    .developers-container {
+        grid-template-columns: 0.85fr;
+    }
 }
 </style>
