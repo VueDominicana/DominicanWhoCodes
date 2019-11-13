f4895661fa306ada973ed57588969fb0636d57b5
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-13 17:43:09 -0400

    Remove .vue extension from all imports

--- src/router.js
+++ src/router.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import DevelopersList from './views/DevelopersList.vue'
+import DevelopersList from './views/DevelopersList'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'developers-list',
-      component: DevelopersList
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+        path: '/',
+        name: 'developers-list',
+        component: DevelopersList
+    }]
 })
