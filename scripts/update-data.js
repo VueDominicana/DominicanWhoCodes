212a049c5c5737d4479e2e820a1bd39dc491876d
Danny Feliz <dannyfeliz08@gmail.com>
2019-11-13 23:58:25 -0400

    Add update data script

---
+++ scripts/update-data.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+
+const sourceURL = "https://raw.githubusercontent.com/AngelGarcia13/DominicanWhoCodes/master/DWC.Blazor/wwwroot/data/developers.json";
+axios({
+    method: "GET",
+    url: sourceURL
+}).then(({ data: developers }) => {
+    let developersFileContent = `{ "developers": ${JSON.stringify(developers)} }`;
+    // We're calling JSON.stringify again just to format with 2 space the whole content of the file.
+    developersFileContent = JSON.stringify(JSON.parse(developersFileContent), null, 2);
+
+    fs.writeFile(path.resolve(__dirname, "../developers.json"), developersFileContent, (err => {
+        if (err) throw err;
+        console.log("Developers data updated correctly.");
+    }))
+}).catch(error => {
+    console.err(error);
+})
