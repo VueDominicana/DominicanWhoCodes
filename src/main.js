8c1201499e5e226c2b0b95377c3ebcf0b41f86c4
Danny Feliz <dannyfeliz08@gmail.com>
2019-11-28 10:06:44 -0400

    Remove boostrap from main

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
-import Vue from 'vue'
-import App from './App'
-import VueLazyLoad from 'vue-lazyload'
-import router from './router'
-import store from './store'
-import "./config"
+import Vue from "vue";
+import App from "./App";
+import VueLazyLoad from "vue-lazyload";
+import router from "./router";
+import store from "./store";
+import "./config";
 import "./config/libs/bootstrap";
 import "./config/libs/fa-icons";
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/style/custom.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/style/custom.css";
 
-Vue.use(BootstrapVue)
 Vue.use(VueLazyLoad, {
-  preLoad: 1.3,
-  attempt: 3,
-})
+    preLoad: 1.3,
+    attempt: 3
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
