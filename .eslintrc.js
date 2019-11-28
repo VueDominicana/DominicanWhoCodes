970a576e9706937d9309b4630848ce1f02695c74
Danny Feliz <dannyfeliz08@gmail.com>
2019-11-28 10:28:22 -0400

    Allow console.log on production mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-undef': "off"
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended"],
+    rules: {
+        "no-console": "warn",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+        "no-undef": "off"
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    }
+};
